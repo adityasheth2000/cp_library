12febecc9710717515d08efb7f22af4e45c9159f
adityasheth2000 <adityasheth2000@googlemail.com>
2021-12-19 16:20:17 +0530

    mobius_transform added in SOS_DP.cpp

--- SOS_DP.cpp
+++ SOS_DP.cpp
@@ -25,3 +25,44 @@ void sum_over_supermask()
 		}
 	}	
 }
+
+
+
+vector<int> mobius_transform(int n, vector<int> f)
+{
+	//BLOG : https://codeforces.com/blog/entry/72488
+	/* Definition : mu[S] = + f[S]  
+							- (all subset of S with exactly one element removed) 
+							+ (all subsets of S with exactly 2 elements removed)
+							- ()
+							+ ()  
+		check definition in blog for more clarity.
+	*/
+	assert(f.size() == (1<<n));
+	// Apply odd-negation transform
+	for(int mask = 0; mask < (1 << n); mask++) {
+		if((__builtin_popcount(mask) % 2) == 1) {
+			f[mask] *= -1;
+		}
+	}
+
+	// Apply zeta transform
+	for(int i = 0; i < n; i++) {
+		for(int mask = 0; mask < (1 << n); mask++) {
+			if((mask & (1 << i)) != 0) {
+				f[mask] += f[mask ^ (1 << i)];
+			}
+		}
+	}
+
+	// Apply odd-negation transform
+	for(int mask = 0; mask < (1 << n); mask++) {
+		if((__builtin_popcount(mask) % 2) == 1) {
+			f[mask] *= -1;
+		}
+	}
+	vector<int> mu(1<<n);
+	for(int mask = 0; mask < (1 << n); mask++)  mu[mask] = f[mask];
+	
+	return mu;
+}
