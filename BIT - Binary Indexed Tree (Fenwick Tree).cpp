36c2a03627262f79ecef21edfa3185c02405e6d2
adityasheth2000 <adityasheth2000@googlemail.com>
2022-08-28 16:17:08 +0530

    updated BIT, removed unuseful code

--- BIT - Binary Indexed Tree (Fenwick Tree).cpp
+++ BIT - Binary Indexed Tree (Fenwick Tree).cpp
@@ -30,26 +30,3 @@ struct BIT
 		return pref(r) - pref(l - 1);
 	}	
 };
-
-/*-----------------------------------------------------*/
-
-struct FenwickTree2D {
-    vector<vector<int>> bit;
-    int n, m;
-
-    // init(...) { ... }
-
-    int sum(int x, int y) {
-        int ret = 0;
-        for (int i = x; i >= 0; i = (i & (i + 1)) - 1)
-            for (int j = y; j >= 0; j = (j & (j + 1)) - 1)
-                ret += bit[i][j];
-        return ret;
-    }
-
-    void add(int x, int y, int delta) {
-        for (int i = x; i < n; i = i | (i + 1))
-            for (int j = y; j < m; j = j | (j + 1))
-                bit[i][j] += delta;
-    }
-};
